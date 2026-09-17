08214d3aa66a3879c4ffe9c7ab13cf81fc08d9ec
so3131 <224105980+so3131@users.noreply.github.com>
2026-09-17 16:59:25 +0200

    Commit 32 : AUTO_INCREMENT manquant sur vg_menu.menu_id

--- app/Database/Schema_VG.sql
+++ app/Database/Schema_VG.sql
@@ -295,7 +295,11 @@ ALTER TABLE `vg_lieu_prestation` ADD PRIMARY KEY (`id`);
 ALTER TABLE `vg_menu` ADD PRIMARY KEY (`menu_id`),
 ADD KEY `fk_menu_theme` (`theme_id`),
 ADD KEY `fk_menu_regime` (`regime_id`);
+--
+-- Index pour la table `vg_menu`
+--
 
+ALTER TABLE `vg_menu` MODIFY `menu_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Index pour la table `vg_menu_plat`
 --
@@ -306,7 +310,6 @@ ADD KEY `plat_id` (`plat_id`);
 -- Index pour la table `vg_password_resets`
 --
 ALTER TABLE `vg_password_resets` ADD PRIMARY KEY (`id`);
-
 
 --
 -- Index pour la table `vg_regime`
